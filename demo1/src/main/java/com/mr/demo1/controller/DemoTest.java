fb6a8183c8a3605371d07831f2f366f83a1c0a97
wang1 <1550553438@qq.com>
2020-04-20 01:45:44 +0800

    新项目1

---
+++ demo1/src/main/java/com/mr/demo1/controller/DemoTest.java
@@ -0,0 +1,11 @@
+package com.mr.demo1.controller;
+
+/**
+ * @author WangWei
+ * @create 2020-04-20-1:40
+ */
+public class DemoTest {
+    public static void main(String[] args) {
+        System.out.println("hello world !!!!");
+    }
+}
