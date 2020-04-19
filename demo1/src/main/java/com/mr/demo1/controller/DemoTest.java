220b409d7f652c4cf3b8f1efc3e41774f1b5dc5d
wang1 <1550553438@qq.com>
2020-04-20 01:47:59 +0800

    新项目1

--- demo1/src/main/java/com/mr/demo1/controller/DemoTest.java
+++ demo1/src/main/java/com/mr/demo1/controller/DemoTest.java
@@ -6,6 +6,6 @@ package com.mr.demo1.controller;
  */
 public class DemoTest {
     public static void main(String[] args) {
-        System.out.println("hello world !!!!");
+        System.out.println("hello world ");
     }
 }
